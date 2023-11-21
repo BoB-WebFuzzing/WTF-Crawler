d2af68910b6686e120ec626a5b1110b075b1d954
MINHYUK PARK <shiro_hacker@kakao.com>
2023-11-21 16:21:15 +0900

    feat / python 실행 파일 생성

---
+++ crawlergo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import simplejson
+import subprocess
+def main():
+    target = "http://testphp.vulnweb.com/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/119.0.0.0 Safari/537.36",
+        "Cookie": "PHPSESSID=4f5c943a8fc68425a469e5184edabf9b; "
+                  "security=low"
+    }
+    cmd = ["bin/crawlergo", "-c", "chrome/linux-119.0.6045.105/chrome-linux64/chrome",
+           "-o", "json", "--output-json", "request_data.json", "--custom-headers", simplejson.dumps(headers),
+           target]
+
+    rsp = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = rsp.communicate()
+    print(output)
+
+    result = simplejson.loads(output.decode().split("--[Mission Complete]--")[1])
+    req_list = result["requestsFound"]
+    for each in req_list:
+        print(each)
+
+if __name__ == '__main__':
+    main()
